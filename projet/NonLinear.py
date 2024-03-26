66743b9f328dc141fe343dd64fb6f081ca2a63f7
unknown <bariskaftancioglu@gmail.com>
2024-03-26 17:47:05 +0100

    partie 2 non linear

---
+++ projet/NonLinear.py
@@ -0,0 +1,38 @@
+from Module import *
+
+class Tanh(Module):
+    def __init__():
+        super.__init__()
+    def forward(self, X):
+        return np.tanh(X)
+
+    def zero_grad(self):
+        pass  #No gradient 
+
+    def backward_update_gradient(self, input, delta):
+        pass  # No gradient to update in TanH
+
+    def backward_delta(self, input, delta):
+        return delta * (1 - self(input) ** 2)
+
+    def update_parameters(self, learning_rate):
+        pass  # No parameters to update in TanH
+
+class Sigmode(Module):
+    def __init__():
+        super.__init__()
+    def forward(self, X):
+        return 1 / (1 + np.exp(-X))
+    
+    def zero_grad(self):
+        pass  #No gradient
+
+    def backward_update_gradient(self, input, delta):
+        pass  # No gradient to update in Sigmoid
+
+    def backward_delta(self, input, delta):
+        sig_X = self(input)
+        return delta * sig_X * (1 - sig_X)
+
+    def update_parameters(self, learning_rate):
+        pass  # No parameters to update in Sigmoid
